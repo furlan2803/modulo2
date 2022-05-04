9232886c4c53173293d602dcf9c27f8a1fb8f7e7
Giovanna Furlan Torres <giovanna.torres@sou.inteli.edu.br>
2022-05-04 07:39:40 -0300

    Update Tutorial - Semana 3

---
+++ 04_AUT_EST_EX_OBRIGATORIOS/Semana 3/Exe.LV/style.js
@@ -0,0 +1,30 @@
+const btn = document.getElementById('btn');
+
+function calculo(){
+    // alert( 'teste click') - Teste do clique do botão
+    const valor = document.getElementById('valor').value;
+    const resultado = document.getElementById('resultado');
+    const resultado2 = document.getElementById('resultado2');
+
+    if (valor !== ''){
+        // alert('preenchido') - Teste Valor do form preenchido
+        const valorHmax = (valor * valor / (2 * 10)).toFixed(2);
+        const valorTs = (valor / 10).toFixed(2);
+
+        resultado.textContent = valorHmax;
+        resultado2.textContent = valorTs;
+    }
+    else{
+        alert('Preencha o campo de "Velocidade Inicial"')
+    }
+}
+
+btn.addEventListener('click', calculo)
+
+
+
+// valorHmax = vo^2 / 2*g
+
+// g = 10 m/s^2
+
+// ts = v0 / g
