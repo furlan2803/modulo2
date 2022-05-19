03c8746b4a0b9f90513bd496b0f3844459f88a0a
Giovanna Furlan Torres <giovanna.torres@sou.inteli.edu.br>
2022-05-19 09:00:59 -0300

    Update tutorial semana 5

---
+++ 04_AUT_EST_EX_OBRIGATORIOS/Semana 5/exe1/main.js
@@ -0,0 +1,7 @@
+const elements = {
+    inputsnumeros: {
+        primeiro: document.querySelector("form-1"),
+        segundo: document.querySelector("form-2")
+    },
+    radio: document.querySelectorAll("input[type='radio']"),
+}
