6dbffa1292abccc560e27c223d6b481a07db5d4a
Giovanna Furlan Torres <giovanna.torres@sou.inteli.edu.br>
2022-05-20 15:32:24 -0300

    Update Exercício Currículo

---
+++ 03_AUT_EST_ENTREGA/Semana 5/frontend/js/scripts.js
@@ -0,0 +1,32 @@
+window.addEventListener('DOMContentLoaded', event => {
+
+    const sideNav = document.body.querySelector('#sideNav');
+    if (sideNav) {
+        new bootstrap.ScrollSpy(document.body, {
+            target: '#sideNav',
+            offset: 74,
+        });
+    };
+    
+    const navbarToggler = document.body.querySelector('.navbar-toggler');
+    const responsiveNavItems = [].slice.call(
+        document.querySelectorAll('#navbarResponsive .nav-link')
+    );
+    responsiveNavItems.map(function (responsiveNavItem) {
+        responsiveNavItem.addEventListener('click', () => {
+            if (window.getComputedStyle(navbarToggler).display !== 'none') {
+                navbarToggler.click();
+            }
+        });
+    });
+});
+
+$("#myFunction").click(function(){
+    var x = document.getElementById("myDIV");
+    if (x.style.display === "none") {
+      x.style.display = "block";
+    } else {
+      x.style.display = "none";
+    }
+    console.log("clicou")
+});
